380c45efcda9071fe81476fd614aa63b6337155a
Markus Hunsalz <dev@hunsalz.de>
2021-10-27 12:43:47 +0200

    added global metadata

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,13 +5,15 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteTitle: process.env.SITE_TITLE || "Gridsome Starter",
+  siteName: process.env.SITE_NAME || "Gridsome Starter",
   siteDescription: process.env.SITE_DESCRIPTION || "Lorem ipsum ...",
   siteUrl: process.env.SITE_URL,
   pathPrefix: process.env.PATH_PREFIX,
-  author: process.env.AUTHOR || "John Doe",
-  githubUrl: process.env.GITHUB_URL || "https://github.com/octocat",
-  linkedInUrl: process.env.LINKEDIN_URL || "https://www.linkedin.com/in/john-doe",
+  metadata: {
+    author: process.env.AUTHOR || "John Doe",
+    github: process.env.GITHUB_URL || "https://github.com/octocat",
+    linkedin: process.env.LINKEDIN_URL || "https://www.linkedin.com/in/john-doe",
+  },
   plugins: [
     {
       use: "@gridsome/source-filesystem",
