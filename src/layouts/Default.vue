d0be456df213f77025663ee226ec84c5dd924f65
Markus Hunsalz <dev@hunsalz.de>
2021-11-13 14:19:08 +0100

    added mobile friendly nav

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,43 +1,67 @@
 <template>
   <div class="layout">
-    <header class="nav is-fixed sticky top-0 left-0 z-10 w-full">
-      <div class="flex-center flex-rows px-16 py-4 md:px-32 bg-header-background">
-        <div class="text-3xl text-header-color">
-          <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+    <header
+      class="flex flex-col md:flex-row md:items-center nav is-fixed sticky top-0 left-0 z-10 flex px-2 md:px-24 py-4 md:py-6 space-x-2 md:space-x-4 bg-header-background"
+    >
+      <div class="flex flex-row items-center">
+        <!-- mobile button only -->
+        <button class="md:hidden" @click="toggle">
+          <menu-icon class="icon-md" fillColor="var(--color-header-color)" />
+        </button>
+        <div
+          class="flex flex-grow flex-nowrap justify-center md:justify-start text-xl md:text-2xl lg:text-3xl font-semibold tracking-tight text-header-color"
+        >
+          <g-link class="whitespace-nowrap" to="/">{{
+            $static.metadata.siteName
+          }}</g-link>
         </div>
-        <span class="flex-auto"></span>
-        <scrollactive
-          ref="scrollactive"
-          :offset="offset"
-          highlight-first-item
+        <!-- mobile button only -->
+        <ToggleTheme class="md:hidden" />
+      </div>
+      <div :class="open ? 'block' : 'hidden md:block'" class="flex-grow">
+        <scrollactive ref="scrollactive" :offset="offset" highlight-first-item>
+          <ul
+            class="flex flex-col md:flex-row md:justify-center md:space-x-4 text-md md:text-xl lg:text-2xl"
           >
-          <ul class="flex-center flex-rows space-x-4 text-2xl">
             <li>
-              <a href="#about" class="scrollactive-item text-header-color">
+              <a
+                href="#about"
+                class="scrollactive-item text-header-color sm:inline-block sm:mt-0"
+              >
                 about
               </a>
             </li>
             <li>
-              <a href="#experience" class="scrollactive-item text-header-color">
+              <a
+                href="#experience"
+                class="scrollactive-item text-header-color sm:inline-block sm:mt-0"
+              >
                 experience
               </a>
             </li>
             <li>
-              <a href="#work" class="scrollactive-item text-header-color">
+              <a
+                href="#work"
+                class="scrollactive-item text-header-color sm:inline-block sm:mt-0"
+              >
                 work
               </a>
             </li>
             <li>
-              <a href="#contact" class="scrollactive-item text-header-color">
+              <a
+                href="#contact"
+                class="scrollactive-item text-header-color sm:inline-block sm:mt-0"
+              >
                 contact
               </a>
             </li>
           </ul>
         </scrollactive>
-        <ToggleTheme class="pl-4" />
       </div>
+      <!-- desktop button only -->
+      <ToggleTheme class="hidden md:block" />
     </header>
-    <main class="mx-auto px-16 md:px-32 bg-scenery">
+    <main class="mx-auto px-0 md:px-24 bg-scenery">
       <slot />
     </main>
   </div>
@@ -53,15 +77,23 @@ query {
 
 <script>
 import ToggleTheme from "~/components/ToggleTheme"
+import MenuIcon from "vue-material-design-icons/Menu"
 
 export default {
   components: {
+    MenuIcon,
     ToggleTheme,
   },
   data: function() {
     return {
       offset: 84,
+      open: false,
     }
+  },
+  methods: {
+    toggle() {
+      this.open = !this.open
+    },
   },
 }
 </script>
